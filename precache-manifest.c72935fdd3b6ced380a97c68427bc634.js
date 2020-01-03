0a16e3ae53b10b2beb02066cd7004527b0939e2c
ke1echi <mrkelechichinaka@gmail.com>
2020-01-03 18:24:51 +0100

    Updates

---
+++ precache-manifest.c72935fdd3b6ced380a97c68427bc634.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9afaf1f0fe963957d9a336a227bfa066",
+    "url": "/sammychris.github.io/index.html"
+  },
+  {
+    "revision": "47aa1a9596bd64ebbe42",
+    "url": "/sammychris.github.io/static/css/main.c9f5031d.chunk.css"
+  },
+  {
+    "revision": "6ea22432601858ce9ef5",
+    "url": "/sammychris.github.io/static/js/2.27e9ad4f.chunk.js"
+  },
+  {
+    "revision": "47aa1a9596bd64ebbe42",
+    "url": "/sammychris.github.io/static/js/main.27c1b148.chunk.js"
+  },
+  {
+    "revision": "18b65b232e4cdb601712",
+    "url": "/sammychris.github.io/static/js/runtime~main.061a9f35.js"
+  }
+]);
